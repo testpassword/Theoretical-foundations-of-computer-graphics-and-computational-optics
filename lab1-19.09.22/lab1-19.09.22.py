25bb16e37e5848db7491ee04c5f9a1a021f3b7f9
Artemy Kulbako <kulbakowork@mail.ru>
2022-09-21 23:30:31 +0300

    lab1 beta

--- lab1-19.09.22/lab1-19.09.22.py
+++ lab1-19.09.22/lab1-19.09.22.py
@@ -2,12 +2,11 @@ import sys
 import random
 import numpy as np
 import uuid
+from collections import namedtuple
 
-NAME = uuid.uuid4().hex
 
-
-def get_mode() -> list:
-	return ['Plane', 'Point', 'Box']  #default
+# objects which will be created
+Mode = namedtuple('Mode', ['observer', 'light', 'shape', 'is_gonio_light'])
 
 
 #the is no ObserverLibrary in API, so make something similar
@@ -23,6 +22,7 @@ class ObserverLibrary:
 			return None
 
 
+# create scene with nodes at once
 def create_scene(*nodes: Node) -> Scene:
 	scene = Scene()
 	for n in nodes:
@@ -30,15 +30,18 @@ def create_scene(*nodes: Node) -> Scene:
 	return scene
 
 
+# random translate node
 def rand_translate(node: Node) -> Node:
-	# Node.Translate is SM, but Transformation from GUI is M
+	# Node.Translate is centimeters, but Transformation from GUI is meters
 	node.Translate(*np.full(3, random.randint(0, 500))) #[0; 500] range for visibility in default camera
 	return node
 
 
-# todo: gonio observer, gonio light
-
-obs, light, shape = [it[0].GetItem(it[1]) for it in zip([ObserverLibrary, GetLibrary(Light), GetLibrary(Shape)], get_mode())]
+NAME = uuid.uuid4().hex
+mode = Mode('Plane', 'Point', 'Box', True)
+obs, light, shape = [it[0].GetItem(it[1]) for it in zip([ObserverLibrary, GetLibrary(Light), GetLibrary(Shape)], mode)]
+if mode.is_gonio_light:
+	light.gonio = GetLibrary(Gonio).GetFolder('Standard').GetItem('lambertian')
 light.color = SpecLightColor(RGBLightColor(*np.random.rand(3)))
 light.total_flux = random.randint(500, 1000)  #less than 500lm is dim
 #create a large Observer to catch everything
@@ -46,6 +49,7 @@ obs.res = 128, 128
 obs.org = -1000, -1000, 700
 obs.x_side = 2000, 0, 0
 obs.y_side = 0, 2000, 0
+obs.dir = 0, 0, -1000
 obs_n, light_n, shape_n = [it[0](it[1]) for it in zip([ObserverNode, LightNode, MeshNode], [obs, light, shape])]
 for n in [light_n, shape_n]:
 	rand_translate(n)
