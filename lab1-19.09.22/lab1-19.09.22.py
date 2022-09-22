817a0ec8c056f1c1759a08293bb9037f0f77a039
Artemy Kulbako <kulbakowork@mail.ru>
2022-09-22 03:32:54 +0300

    lab1 complete

--- lab1-19.09.22/lab1-19.09.22.py
+++ lab1-19.09.22/lab1-19.09.22.py
@@ -5,10 +5,6 @@ import uuid
 from collections import namedtuple
 
 
-# objects which will be created
-Mode = namedtuple('Mode', ['observer', 'light', 'shape', 'is_gonio_light'])
-
-
 #the is no ObserverLibrary in API, so make something similar
 class ObserverLibrary:
 	@staticmethod
@@ -38,25 +34,37 @@ def rand_translate(node: Node) -> Node:
 
 
 NAME = uuid.uuid4().hex
-mode = Mode('Plane', 'Point', 'Box', True)
-obs, light, shape = [it[0].GetItem(it[1]) for it in zip([ObserverLibrary, GetLibrary(Light), GetLibrary(Shape)], mode)]
-if mode.is_gonio_light:
-	light.gonio = GetLibrary(Gonio).GetFolder('Standard').GetItem('lambertian')
-light.color = SpecLightColor(RGBLightColor(*np.random.rand(3)))
-light.total_flux = random.randint(500, 1000)  #less than 500lm is dim
+obs_lib = ObserverLibrary
+light_lib = GetLibrary(Light)
+shape_lib = GetLibrary(Shape)
+obs = ObserverLibrary.GetItem('Plane')
+point_light = light_lib.GetItem('Point')
+point_light.gonio = GetLibrary(Gonio).GetFolder('Standard').GetItem('lambertian')
+rectangle_light = light_lib.GetItem('Rectangle')
+for l in [point_light, rectangle_light]:
+	l.color = SpecLightColor(RGBLightColor(*np.random.rand(3)))
+	l.total_flux = random.randint(500, 1000)  #less than 500lm is dim
+box = shape_lib.GetItem('Box')
+sphere = Sphere(radius = 100)
 #create a large Observer to catch everything
 obs.res = 128, 128
 obs.org = -1000, -1000, 700
 obs.x_side = 2000, 0, 0
 obs.y_side = 0, 2000, 0
 obs.dir = 0, 0, -1000
-obs_n, light_n, shape_n = [it[0](it[1]) for it in zip([ObserverNode, LightNode, MeshNode], [obs, light, shape])]
-for n in [light_n, shape_n]:
-	rand_translate(n)
-sc = create_scene(obs_n, light_n, shape_n)
+obs_n, point_light_n, rectangle_light_n, box_n, sphere_n = [
+	it[0](it[1]) for it in zip(
+		[ObserverNode, LightNode, LightNode, MeshNode, MeshNode], 
+		[obs, point_light, rectangle_light, box, sphere])
+]
+point_light_n.Translate(500, 500, 750)
+rectangle_light_n.Translate(-500, 500, 750)
+for s in [box_n, sphere_n]:
+	rand_translate(s)
+sc = create_scene(obs_n, point_light_n, rectangle_light_n, box_n, sphere_n)
 LoadScene(sc)
 render_params = sc.RenderParams()
-render_params.res = 128, 128
+render_params.res = 256, 256
 imaps_params = sc.IMapsParams()
 imaps_params.SetObserverAsAccSource(obs_n)
 notebook = sc.Notebook()
